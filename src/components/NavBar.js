b942c7e6d7a853a876a7653287428503dd86499d
andrewdownie <andrew_dev@outlook.com>
2018-02-13 20:18:57 -0500

    Converted NavBar.js back to a pure function.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,27 +2,23 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import React, { Component } from 'react';
 
 
-class NavBar extends Component {
-	constructor(props){
-		super(props); 
-		this.state = {};
-	}
+const NavBar = (props) => {
 
-	signInButtonClick = () => {
-		var auth2 = this.props.gapi.auth2.getAuthInstance();
+	const signInButtonClick = () => {
+		var auth2 = props.gapi.auth2.getAuthInstance();
 
-		if(this.props.isSignedIn){
+		if(props.isSignedIn){
 			auth2.signOut();
 			console.log("Signed out");
 		}
 		else{
 			//TODO: sign in here
 			console.log("Signing in...");
-			var gapi = this.props.gapi;
+			var gapi = props.gapi;
 			auth2.signIn().then(
 				() => {
 					gapi.client.load('sheets', 'v4', () => {
-						this._readSheetList(gapi);
+						_readSheetList(gapi);
 					})
 				}
 			);
@@ -32,7 +28,7 @@ class NavBar extends Component {
 	// TODO: should this stuff go into a component?
 	// TODO: ya it should go into the body right? It shouldn't be chained, it should just check the prop to see if things are signed in,
 	//			if they are signed in then it should perform the request to load contents into the body, the navbar shouldn't care about the actual data from the sheet
-	_readSheetList(gapi) {
+	const _readSheetList = (gapi) => {
 		gapi.client.sheets.spreadsheets.values.get({
 			spreadsheetId: '1u9ljq0razYyn-yTou6e8yAoHuLnCdGKU_a7URpbeSvg',
 			range: 'A1:E1',
@@ -57,34 +53,32 @@ class NavBar extends Component {
 		});
 	}
 
-	signInButtonText = () => {
-		if(this.props.isSignedIn){
+	const signInButtonText = () => {
+		if(props.isSignedIn){
 			return <span>Sign Out</span>
 		}
 		return <span>Sign In</span>
 	}
 
-	render(){
-		return (
-			<Navbar>
-				<Navbar.Header>
-					<Navbar.Brand>
-						<a href="#home">React-Bootstrap</a>
-					</Navbar.Brand>
-				</Navbar.Header>
-				<Nav>
-					<NavItem eventKey={1} href="#">
-						Link
-					</NavItem>
-				</Nav>
-				<Nav pullRight>
-					<NavItem onClick={this.signInButtonClick} eventKey={2} href="#">
-						{this.signInButtonText()}
-					</NavItem>
-				</Nav>
-			</Navbar>
-		);
-	}
+	return (
+		<Navbar>
+			<Navbar.Header>
+				<Navbar.Brand>
+					<a href="#home">React-Bootstrap</a>
+				</Navbar.Brand>
+			</Navbar.Header>
+			<Nav>
+				<NavItem eventKey={1} href="#">
+					Link
+				</NavItem>
+			</Nav>
+			<Nav pullRight>
+				<NavItem onClick={signInButtonClick} eventKey={2} href="#">
+					{signInButtonText()}
+				</NavItem>
+			</Nav>
+		</Navbar>
+	);
 
 }
 
