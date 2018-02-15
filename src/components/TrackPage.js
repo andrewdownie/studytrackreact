16a5ddc00dd46ba17c4eae310e780679906dff8d
andrewdownie <andrew_dev@outlook.com>
2018-02-14 19:56:57 -0500

    I've created my first promise. It's pretty dank.

--- src/components/TrackPage.js
+++ src/components/TrackPage.js
@@ -94,45 +94,80 @@ class TrackPage extends Component {
 
     }
 
-    render(){
-        console.log("Sheet data is:");
-        console.log(this.state.sheetData);
+    _weekOfYear(){
+        var d = new Date();
+        var dayNum = d.getUTCDay() || 7;
+        d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+        return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+    }
 
-        //TODO: I don't know where to put this atm, should I create a component full of static vars that holds all the google connection setup data?
-        var USERDATA_SHEET_NAME = "StudyTrackUserData"
+    _sheetName(){
+        var d = new Date();
+        return d.getFullYear().toString().substr(-2) + this._weekOfYear();
+    }
 
-        if(this.props.isSignedIn && this.state.sheetData == null){//TODO: is this the best way to make sure the sheet loading runs once?
+    _lastWeeksSheetName(){
+        var d = new Date();
+        return d.getFullYear().toString().substr(-2) + (this._weekOfYear() - 1);
+    }
+
+
+    _findUserDataSheet(){
+        return new Promise((resolve, reject) => {
+
+            //TODO: I don't know where to put this atm, should I create a component full of static vars that holds all the google connection setup data?
+            var USERDATA_SHEET_NAME = "StudyTrackUserData"
             console.log("signed in, load sheets now");
 
-            //TODO: parallel promise??
-            //TODO: need to load the drive api here... need to find the signature to do this
             this.props.gapi.client.load('drive', 'v2', () => {
-                //TODO: load all of the sheets this app has access to
-                //TODO: then grab the (?newest?) one, and feed it into the readSheetData call
 
                 
                 var listRequest = this.props.gapi.client.drive.files.list();
                 
                 console.log("RESPONSE OF LOADING FILES IS:");
                 //TODO: add parameter for name of sheet
-                var lastSheet;
+                var sheetID = null;
                 listRequest.execute((response) => {
-                    for(var i = 0; i < response.files.length; i++){
-                        //console.log(response.files[i]);
-                        lastSheet = response.files[i].id;
+                    var len = response.files.length;
+                    if(len > 0){
+                        for(var i = 0; i < response.files.length; i++){
+                            //console.log(response.files[i]);
+                            if(response.files[i].name == USERDATA_SHEET_NAME){
+                                sheetID = response.files[i].id;
+                            }
+                        }
+                        console.log("last sheet id is: " + sheetID);
                     }
 
-                    console.log("last sheet id is: " + lastSheet);
-                    this.props.gapi.client.load('sheets', 'v4', () => {
-                        this._readSheetData(lastSheet);
-                    });
+                    if(sheetID == null){
+                        //TODO: create a userdata spreadsheet for the user somewhere around here
+                        console.log("no userdata sheet found, creating a new one now...");
+                        reject(sheetID);
+                    }
+                    else{
+                        resolve(sheetID);
+                    }
                 });
-
-
-
             });
 
+        });
+    }
 
+
+    render(){
+        console.log("Sheet data is:");
+        console.log(this.state.sheetData);
+
+
+        if(this.props.isSignedIn && this.state.sheetData == null){//TODO: is this the best way to make sure the sheet loading runs once?
+            this._findUserDataSheet().then((sheetID) =>{
+
+                this.props.gapi.client.load('sheets', 'v4', () => {
+                    this._readSheetData(sheetID);
+                });
+
+            });
         }
 
         var gapi = this.props.gapi;
