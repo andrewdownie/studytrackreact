66279b74398d99d84a8494279a88b728617cbe85
andrewdownie <andrew_dev@outlook.com>
2018-02-19 15:06:33 -0500

    Now have mock custom data being passed in, need to pass real data in now.

--- src/components/Today.js
+++ src/components/Today.js
@@ -2,27 +2,66 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Chart } from 'react-google-charts';
 
-const Today= (props) => {
+
+const calcChartify = (projectInfo) => {
+    // Takes json data from the sheet, and repacks it to plug into google charts
+
+    var title = projectInfo.title;
+    var studied = projectInfo.studied;
+    var min = projectInfo.min;
+    var ideal = projectInfo.ideal;
+
+    var outMin, outIdeal;
+
+    if(studied > min){
+        outMin = 0;
+        outIdeal = ideal - (studied - min);
+    }
+    else{
+        outMin = min - studied;
+        outIdeal = ideal;
+    }
+    
+    //return [title, studied, outMin, outIdeal];//TODO: things aren't being calculated correctly atm...
+    return [title, 200, 500, 1000, ""];
+}
+
+const dataChartify = (chartData) => {
+    //Given a list of projects, returns a json object ready to be passed into a google chart
+    var header = ['Time Tracking', 'Studied', 'Min', 'Ideal', { role: 'annotation' } ];
+
+    var output = [];
+    output.push(header);
+
+    for(var i = 0; i < chartData.projects.length; i++){
+        output.push(calcChartify(chartData.projects[i]));
+    }
+
+    return output;
+}
+
+const Today = (props) => {
+
     var todayTest = "";
+    var chartData = [];
 
     if(props.todaysData != null){
         todayTest = props.todaysData.projects[1].title;
+        chartData = dataChartify(props.todaysData);
+
+        console.log(chartData);
+        console.log(exampleChartData);
     }
+
+
     
     return(
+        //TODO: make this chart load data dynamically
         <div>
             <div className={'my-pretty-chart-container'}>
                 <Chart
                 chartType="ColumnChart"
-                data={
-                    [
-                        ['API Category', 'Social', 'Music', 'File Sharing', 'Storage',
-                         'Weather', { role: 'annotation' } ],
-                        ['2011', 98, 53, 12, 16, 6, ''],
-                        ['2012', 151, 34, 26, 36, 49, ''],
-                        ['2013', 69, 27, 22, 17, 15, ''],
-                      ]
-                }
+                data={chartData}
                 options={{isStacked: true, legend: { position: 'top', maxLines: 3, textStyle: {color: 'black', fontSize: 16 } }}}
                 graph_id="ColumnChart"
                 width="100%"
