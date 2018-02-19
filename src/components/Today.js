0cf017803a7d8d15881ff8356216156edf9afd13
andrewdownie <andrew_dev@outlook.com>
2018-02-19 14:23:33 -0500

    Got a simple static stacked-column chart working.

--- src/components/Today.js
+++ src/components/Today.js
@@ -11,6 +11,31 @@ const Today= (props) => {
     
     return(
         <div>
+            <div className={'my-pretty-chart-container'}>
+                <Chart
+                chartType="ColumnChart"
+                data={
+                    [
+                        ['API Category', 'Social', 'Music', 'File Sharing', 'Storage',
+                         'Weather', { role: 'annotation' } ],
+                        ['2011', 98, 53, 12, 16, 6, ''],
+                        ['2012', 151, 34, 26, 36, 49, ''],
+                        ['2013', 69, 27, 22, 17, 15, ''],
+                      ]
+                }
+                options={{isStacked: true, legend: { position: 'top', maxLines: 3, textStyle: {color: 'black', fontSize: 16 } }}}
+                graph_id="ColumnChart"
+                width="100%"
+                height="400px"
+                legend_toggle
+            />
+
+            </div>
+        </div>
+
+
+
+        /*<div>
             <h2>This is Today.js</h2>
             <p>Todays data next:</p>
             <p>{todayTest}</p>
@@ -23,11 +48,12 @@ const Today= (props) => {
                 graph_id="ScatterChart"
                 width="100%"
                 height="400px"
+                stacked="true"
                 legend_toggle
             />
 
             </div>
-        </div>
+        </div>*/
     );
 };
 
