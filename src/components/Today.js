e28ac48a6da435824e26984d42bb1945d4c7afd1
andrewdownie <andrew_dev@outlook.com>
2018-02-19 13:42:46 -0500

    Added a google column chart using the react-google-charts package.

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
 const Today= (props) => {
     var todayTest = "";
@@ -12,6 +14,19 @@ const Today= (props) => {
             <h2>This is Today.js</h2>
             <p>Todays data next:</p>
             <p>{todayTest}</p>
+
+            <div className={'my-pretty-chart-container'}>
+                <Chart
+                chartType="ColumnChart"
+                data={[['Age', 'Weight'], [8, 12], [4, 5.5]]}
+                options={{}}
+                graph_id="ScatterChart"
+                width="100%"
+                height="400px"
+                legend_toggle
+            />
+
+            </div>
         </div>
     );
 };
